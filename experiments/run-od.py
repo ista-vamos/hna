b85411a1331ca79bbb1ef15c715043908c0f6db9
Marek Chalupa <mchqwerty@gmail.com>
2024-05-29 03:06:33 +0200

    expr: refactor code a bit

--- experiments/run-od.py
+++ experiments/run-od.py
@@ -11,6 +11,8 @@ from os import chdir, makedirs, listdir
 from random import randrange
 from statistics import mean, stdev
 
+from traces import *
+
 SRCDIR=f"{dirname(sys.argv[0])}/../"
 HNL_SCRIPT=f"{SRCDIR}/hnl.py"
 TRIALS=10 # how many times run the monitor on a given input
@@ -88,110 +90,6 @@ def measure(inputs, timeout=30):
         values.append(parse(retval, is_timeout, outs.splitlines(), errs.splitlines()))
 
     return values
-
-def gen_traces_single_input(alphabet, outdir, num, length):
-    """
-    Generate traces that are OD. The input is in the first event
-    and then stutters, if two traces share the same input,
-    then one trace's outputs are a prefix of the other
-    """
-    makedirs(outdir, exist_ok=True)
-
-    all_traces = []
-    traces = {}
-    for i in range(1, num + 1):
-        a_i = alphabet[randrange(0, len(alphabet))]
-        trace_in = []
-        trace_out = []
-
-        trace_in = [a_i] * length
-        if str(trace_in) in traces:
-            trace_out = traces[str(trace_in)]
-        else:
-            for n in range(length):
-                a_o = alphabet[randrange(0, len(alphabet))]
-                trace_out.append(a_o)
-            traces[str(trace_in)] = trace_out
-        all_traces.append((trace_in, trace_out))
-
-    for n, t in enumerate(all_traces):
-        with open(f"{outdir}/{n}.csv", "w") as f:
-            f.write("in,out\n")
-            for i in range(0, max(len(t[0]), len(t[1]))):
-                i1 = i if i < len(t[0]) else len(t[0]) - 1
-                i2 = i if i < len(t[1]) else len(t[1]) - 1
-                f.write(f"{t[0][i1]}, {t[1][i2]}\n")
-
-
-def gen_traces_two_inputs(alphabet, outdir, num, length):
-    """
-    Generate traces that are OD. The input is in the first event
-    and then stutters, if two traces share the same input,
-    then one trace's outputs are a prefix of the other
-    """
-    makedirs(outdir, exist_ok=True)
-
-    all_traces = []
-    traces = {}
-    for i in range(1, num + 1):
-        a_i1 = alphabet[randrange(0, len(alphabet))]
-        a_i2 = alphabet[randrange(0, len(alphabet))]
-        trace_in = []
-        trace_out = []
-
-        trace_in = [a_i1] + [a_i2] * (length - 1)
-        if str(trace_in) in traces:
-            trace_out = traces[str(trace_in)]
-        else:
-            for n in range(length):
-                a_o = alphabet[randrange(0, len(alphabet))]
-                trace_out.append(a_o)
-            traces[str(trace_in)] = trace_out
-        all_traces.append((trace_in, trace_out))
-
-    for n, t in enumerate(all_traces):
-        with open(f"{outdir}/{n}.csv", "w") as f:
-            f.write("in,out\n")
-            for i in range(0, max(len(t[0]), len(t[1]))):
-                i1 = i if i < len(t[0]) else len(t[0]) - 1
-                i2 = i if i < len(t[1]) else len(t[1]) - 1
-                f.write(f"{t[0][i1]}, {t[1][i2]}\n")
-
-def gen_traces_rand_inputs(alphabet, outdir, num, length):
-    """
-    Generate traces that are OD. The input is in the first event
-    and then stutters, if two traces share the same input,
-    then one trace's outputs are a prefix of the other
-    """
-    makedirs(outdir, exist_ok=True)
-
-    all_traces = []
-    traces = {}
-    for i in range(1, num + 1):
-        trace_in = []
-        trace_out = []
-
-        for n in range(length):
-            a_i = alphabet[randrange(0, len(alphabet))]
-            trace_in.append(a_i)
-
-        if str(trace_in) in traces:
-            trace_out = traces[str(trace_in)]
-        else:
-            for n in range(length):
-                a_o = alphabet[randrange(0, len(alphabet))]
-                trace_out.append(a_o)
-            traces[str(trace_in)] = trace_out
-        all_traces.append((trace_in, trace_out))
-
-    for n, t in enumerate(all_traces):
-        with open(f"{outdir}/{n}.csv", "w") as f:
-            f.write("in,out\n")
-            for i in range(0, max(len(t[0]), len(t[1]))):
-                i1 = i if i < len(t[0]) else len(t[0]) - 1
-                i2 = i if i < len(t[1]) else len(t[1]) - 1
-                f.write(f"{t[0][i1]}, {t[1][i2]}\n")
-
 
 
 # numbers on 3 bits
