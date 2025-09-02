5f4840452104b8f15579d21a6a85c5b4e8c7c82b
Marek Chalupa <mchqwerty@gmail.com>
2025-09-02 16:05:53 +0200

    Add a docstring

---
+++ rvhyno/hnl/__init__.py
@@ -0,0 +1,21 @@
+"""
+This package takes care of parsing hypernode logic and generating monitors for this logic.
+
+Several versions of the logic are supported:
+ - Extended Hypernode Logic (eHL) :ref:`[1] <references>`
+ - Symbolic Hypernode Logic (sHL) :ref:`[2] <references>`
+
+.. _references:
+References
+-----
+
+
+ [1] Chalupa, M., Henzinger, T.A., Oliveira da Costa, A. (2025).
+     **Monitoring Extended Hypernode Logic**.
+     In: Kosmatov, N., Kovács, L. (eds) Integrated Formal Methods. IFM 2024.
+     Lecture Notes in Computer Science, vol 15234. Springer, Cham.
+     https://doi.org/10.1007/978-3-031-76554-4_9
+ [2] Chalupa, M., Henzinger, T.A., Olivera da Costa, A. (2025).
+     **Monitoring Hypernode Logic over Infinite Domains**.
+     RV 2025. to appear.
+"""
